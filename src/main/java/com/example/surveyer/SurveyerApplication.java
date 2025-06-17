00e0ff701ad27b8e8c38e2bb6a064cc7eb8d8857
munna-yadav <mail.munna50@gmail.com>
2025-06-17 22:03:13 +0530

    setup

---
+++ src/main/java/com/example/surveyer/SurveyerApplication.java
@@ -0,0 +1,13 @@
+package com.example.surveyer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyerApplication.class, args);
+	}
+
+}
