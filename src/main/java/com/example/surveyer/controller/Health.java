81f7afe6abc304d2cd6f509c8a032477f3b380d0
munna-yadav <mail.munna50@gmail.com>
2025-06-17 22:01:52 +0530

    initial commit

---
+++ src/main/java/com/example/surveyer/controller/Health.java
@@ -0,0 +1,12 @@
+package com.example.surveyer.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Health {
+    @GetMapping("/health")
+    public String health(){
+        return  "OK";
+    }
+}
